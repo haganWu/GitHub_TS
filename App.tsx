d2b24d8a33f522ee3d95a38865af6cd149ee2ed9
haganWu <10173167@qq.com>
2021-07-19 14:41:08 +0800

    iOS端 -- 适配iOS刘海屏

--- App.tsx
+++ App.tsx
@@ -10,8 +10,33 @@
 
 import React from "react";
 import Navigator from "./src/navigator";
+import {
+  Platform,
+  SafeAreaView,
+  NativeModules,
+  StatusBar, StyleSheet,
+} from "react-native";
 
+const { StatusBarManager } = NativeModules;
 const App = () => {
-  return <Navigator />;
+  let statusBarHeight;
+  if (Platform.OS === "ios") {
+    StatusBarManager.getHeight((height: number) => {
+      console.log("height11:", height);
+      statusBarHeight = height;
+    });
+  } else {
+    statusBarHeight = StatusBar.currentHeight;
+  }
+  return (
+    <SafeAreaView style={[styles.container, { marginTop: statusBarHeight }]}>
+      <Navigator />
+    </SafeAreaView>
+  );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 export default App;
